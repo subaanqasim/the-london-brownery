2d9c6219d98838122da64502a9bdf13458c7722c
Subaan Qasim <subaan.qasim@gmail.com>
2022-12-20 11:58:18 +0000

    initial commit

---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  arrowParens: "always",
+  endOfLine: "lf",
+  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+};
